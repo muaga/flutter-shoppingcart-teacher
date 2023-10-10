17aab9ee05875f04ef5fbee10624f537115b5bdc
muaga <more9494@naver.com>
2023-10-10 12:18:16 +0900

    cㅑãimage 배열에 담기

---
+++ lib/components/shoppingcart_header.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+// 해당 페이지가 stateful인 이유 = 상태가 있는 위젯
+class ShoppingCartPage extends StatefulWidget {
+  const ShoppingCartPage({super.key});
+
+  @override
+  State<ShoppingCartPage> createState() => _ShoppingCartPageState();
+}
+
+class _ShoppingCartPageState extends State<ShoppingCartPage> {
+  // image id 초기화
+  int selectedId = 0;
+
+  // selectedPic 배열에 이미지 담기
+  List<String> selectedPic = [
+    "assets/p1.jpeg",
+    "assets/p2.jpeg",
+    "assets/p3.jpeg",
+    "assets/p4.jpeg"
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox();
+  }
+}
